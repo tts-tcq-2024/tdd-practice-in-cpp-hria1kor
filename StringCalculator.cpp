350cee66011fe558d7d720dd6a8b9325ad73fb56
hria1kor <hrithikranjan9304@gmail.com>
2024-07-10 15:13:49 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,13 @@
+#include "StringCalculator.h"
+#include <string>
 
+bool StringCalculator::IsNullOrEmpty(const std::string* numbers) {
+    return numbers == nullptr || numbers->empty();
+}
+
+int StringCalculator::add(const std::string& numbers) {
+    if (IsNullOrEmpty(&numbers)) {
+        return 0;
+    }
+    return -1;
+}
