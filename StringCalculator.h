debb21a8d2625523efe160da718251f3660d32c7
hria1kor <hrithikranjan9304@gmail.com>
2024-07-10 15:09:09 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,18 @@
+int isEmptyString(const char *str)
+{
+    // Check if the string is NULL or the first character is '\0'
+    if(str == NULL || *str == '\0')
+    {   
+        return 1;
+    }
+}
 
+int add(const char* input)
+{
+    // Check if the string is NULL or the first character is '\0'
+   if(isEmptyString(input))
+    {
+   return 0;
+    }
+    return -1;
+}
